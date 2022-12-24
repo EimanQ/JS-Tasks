5bb94bdfd02f3285fec1b93bb96ef9fb8c234552
Yanis Khenilane <luckyheya@gmail.com>
2022-12-24 23:49:00 +0100

    update

---
+++ TypeScript/7 task — копия/app.ts
@@ -0,0 +1,20 @@
+const array: { id: number; title: string; count: number; price: number }[] = [
+  { id: 1, title: "Часы", count: 10, price: 500 },
+  { id: 2, title: "Смартфон", count: 33, price: 1500 },
+  { id: 3, title: "Моноблок", count: 6, price: 2200 },
+  { id: 4, title: "Ноутбук", count: 13, price: 3000 },
+  { id: 5, title: "Планшет", count: 22, price: 2100 },
+];
+
+let result: {}[] = [];
+let maxTotal = 0;
+
+for (let i: number = 0; i < array.length; i++) {
+  const totalPrice: number = array[i].price * array[i].count;
+  if (maxTotal < totalPrice) {
+    result = [array[i]];
+    maxTotal = totalPrice;
+  } else continue;
+}
+
+console.log(result);
