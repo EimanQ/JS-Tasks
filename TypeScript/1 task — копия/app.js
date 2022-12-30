8152ee74da6c73430d3b4a46e31fc064891aa1f0
Yanis Khenilane <luckyheya@gmail.com>
2022-12-30 23:55:35 +0100

    updt

--- TypeScript/1 task — копия/app.js
+++
@@ -1,9 +0,0 @@
-var array = [
-    { id: 1, title: "Часы", count: 10, price: 500 },
-    { id: 2, title: "Смартфон", count: 33, price: 1500 },
-    { id: 3, title: "Моноблок", count: 6, price: 2200 },
-    { id: 4, title: "Ноутбук", count: 13, price: 3000 },
-    { id: 5, title: "Планшет", count: 22, price: 2100 },
-];
-var result = array.filter(function (el) { return el.count >= 10; });
-console.log(result);
