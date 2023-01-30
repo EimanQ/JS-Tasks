c6955f4a01cb8cbfa80516de8a7bc8b7867782bf
Yanis Khenilane <luckyheya@gmail.com>
2023-01-30 23:07:46 +0100

    updts

--- TypeScript/8 task — копия/app.js
+++
@@ -1,8 +0,0 @@
-var arrays = ["HI", "code", "Hi", "ilikecode", "ts", "hi"];
-var res = [];
-for (var i = 0; i < arrays.length; i++) {
-    if (!res.includes(arrays[i]))
-        res.push(arrays[i]);
-    continue;
-}
-console.log(res);
